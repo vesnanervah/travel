33cb70114d5495127b6d83f29392b9d0c372f7e5
Nervniy <c.zwerew2012@gmail.com>
2023-02-19 15:27:44 +0300

    feat: add >769p carousel function, add 390-769p adaptive for few sections and burger-like nav

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ let bcgHeight = cards.offsetHeight+footer.offsetHeight;
 doubleBcg.style.top = bcgCoordY+'px';
 doubleBcg.style.height = bcgHeight+'px';
 //
+
+//burger and hidden mobile menu
+let burger = document.querySelector('.burger');
+let hiddenMenu = document.querySelector('.mobile-hidden-menu');
+let closeMenuBtn = document.querySelector('.close-menu');
+burger.addEventListener('click', function(e){
+    hiddenMenu.classList.toggle('menu-is-open');
+})
+closeMenuBtn.onclick = function(e){
+    hiddenMenu.classList.toggle('menu-is-open');
+}
+//
+
+//carousel on >768p res
+let bigCarousel = document.querySelector('.img-list');
+let bigCarouselBtns = document.querySelectorAll('.carousel-btn');
+
+function changeBigCarouselPos(newActiveBtn){
+    bigCarouselBtns.forEach(function(btn){
+        if(btn.classList.contains('active-carousel-btn')){
+            btn.classList.remove('active-carousel-btn');
+        }
+    })
+    newActiveBtn.classList.add('active-carousel-btn');
+    if(newActiveBtn.classList.contains('btnMiddle')){
+        bigCarousel.style.left = '0px';
+    }
+    if(newActiveBtn.classList.contains('btnLeft')){
+        bigCarousel.style.left = '54%';
+    }
+    if(newActiveBtn.classList.contains('btnRight')){
+        bigCarousel.style.left = '-54%';
+    }
+}
+
+
+bigCarouselBtns.forEach(function(btn){
+    btn.onclick = function(e){
+        changeBigCarouselPos(btn);
+    }
+})
+
